639983c03c703401bb528d0c4462196748d2c8b9
League Student <league@AdministorsiMac.attlocal.net>
2019-06-26 18:24:51 -0700

    Magic8Ball.java  UNFINSIHED
    :unamused:  :

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -9,7 +9,7 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
-		
+		for(int i = 0;i<10;i++) {
 		Random randomMaker = new Random();
 		
 		 int randomNumber = randomMaker.nextInt(5);
@@ -18,10 +18,21 @@ public class Validation {
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
 if(randomNumber== 0) {
-	System.out.println("Did you know that Earth was habitable till approximately 4.5 Billion");
-} else if (randomNumber) {
-	System.out.println("Despite Jurrasic World having the mosasaurus at 100 or so feet a real mosasaur was only 30-40 people.");
+	System.out.println("Did you know that Earth wasn't habitable till approximately 4.5 Billion years ago");
+} else if (randomNumber==1) {
+	System.out.println("Despite Jurrasic World having the mosasaurus at 100 or so feet, the real largest mosausaur at most was only 30-40 feet.");
 }
+else if(randomNumber==2) {
+	System.out.println("Fun fact jaekelopterus rhenaniae the largest extinct scorpion coming in at 8.2 feet in length makes the emperor scorpion's 9 inches look puny in comparison");
+}
+else if(randomNumber==3) {
+	System.out.println("During the Carboniferous Period creepy crawlies. If you had entomophobia it would have beeen a real nightmare for you. Spiders the size of your face a huge dragonfly known as Meganeura with a wingspan of 70 centimeters plus a huge millipede called arthropleura armata, at seven feet in length and 1.5 feet you would want to find that in your house.");
+
+}
+else if(randomNumber==4) {
+	System.out.println("There have been Five mass extinctions. The most well known being the extinction of the dinosaurs that took place about 65 million years ago. This is event is known as the Cretaceous Palogene extinction event. The Triassic, Permian-Triassic, late Devonian, and the Ordovician-Silurian are the other mass extinctions.");
+}
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program.They will like it('<>')
--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -6,18 +6,25 @@ package _05_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
+public static void main(String[] args) {
+	
 
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)" 
+Random randomMaker = new Random();
+int randomNumber = randomMaker.nextInt(4);
 	// 3. Print out this variable
-
+System.out.println(randomNumber);
 	// 4. Get the user to enter a question for the 8 ball
-
+JOptionPane.showInputDialog("ASK THE AmAzInG eIgHt BaLl a yes or no question");
 	// 5. If the random number is 0
-
+if(randomNumber==0) {
+	
+}
 	// -- tell the user "Yes"
 
 	// 6. If the random number is 1
@@ -32,4 +39,5 @@ public class Magic8Ball {
 
 	// -- write your own answer
 
+}
 }
